4f3b9a21b55164767119537c5afec10bbc6d41f3
lakshan <nipunajith235@gmail.com>
2024-11-27 15:51:48 +0530

    "Removed AuroraBackground and globe-related code, added Navbar component, and replaced page content with Hero and About sections."

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/navbar/Navbar";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="fixed inset-x-0 top-5">
+
+        <Navbar />
+        </div>
+
+        {children}
+       
+      </body>
     </html>
   );
 }
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-[100vh]'>Hero</div>
+  )
+}
+
+export default Hero
